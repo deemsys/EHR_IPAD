db48a7db698bf763f666cba7d2ca521e72e49517
deemsys <sangeetha_d@deemsysinc.com>
2013-10-03 13:35:55 +0530

    first commit

---
+++ EHR/ViewAppAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ViewAppAppDelegate.h
+//  EHR
+//
+//  Created by DeemSysInc on 03/10/13.
+//  Copyright (c) 2013 DeemSysInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewAppAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EHR/ViewAppViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewAppViewController.m
+//  EHR
+//
+//  Created by DeemSysInc on 03/10/13.
+//  Copyright (c) 2013 DeemSysInc. All rights reserved.
+//
+
+#import "ViewAppViewController.h"
+
+@interface ViewAppViewController ()
+
+@end
+
+@implementation ViewAppViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
